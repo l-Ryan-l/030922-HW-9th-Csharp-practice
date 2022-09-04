840b15d32234a6728047cc45cae66bd3695baf32
Ryan <llryanll@yandex.ru>
2022-09-04 20:03:50 +0300

    task3 - solved

---
+++ task3-SummBetweenMandN/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Задача 66: Задайте значения M и N. Напишите программу, которая найдет сумму натуральных 
+элементов в промежутке от M до N.
+
+M = 2; N = 15 -> 120
+M = 4; N = 8 -> 30 
+*/
+
+ int SummBetweenMandN (int n, int m)
+{
+    
+    if (m > n)
+    {
+        return 0;
+    }
+    return m + SummBetweenMandN(n, m + 1);
+}
+
+int result = SummBetweenMandN (15, 2);
+System.Console.WriteLine(result);
